07ee9011b99aebf834384251cff910728196aec5
francoisxavierdesaintjean <francois-xavier1.leprovost-de-saint-jean@supinfo.com>
2025-03-10 11:35:51 +0100

    feat: add hotel capacity input field to admin page hotel creation form

--- src/components/admin-page.jsx
+++ src/components/admin-page.jsx
@@ -9,7 +9,8 @@ import {
     SearchIcon,
     SettingsIcon,
     StarIcon,
-    UsersIcon
+    UsersIcon,
+    PersonStandingIcon,
 } from "lucide-react"
 import axios from "axios";
 import {useAuth} from "../context/AuthContext.jsx";
@@ -36,7 +37,8 @@ export default function AdminPage() {
         description: "",
         rating: 3,
         picture: "",
-        amenities: []
+        amenities: [],
+        capacity: 1,
     })
 
     const handleSearchUsers = (e) => {
@@ -680,6 +682,26 @@ export default function AdminPage() {
                                                 <option value="pet-friendly">Animaux acceptés</option>
                                                 <option value="pool">Piscine</option>
                                             </select>
+                                        </div>
+                                        <div className="relative">
+                                            <label htmlFor="hotelCapacity" className="sr-only">
+                                                Capacité
+                                            </label>
+                                            <div
+                                                className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                                <PersonStandingIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                                            </div>
+                                            <input
+                                                id="hotelCapacity"
+                                                name="hotelCapacity"
+                                                type="number"
+                                                min="1"
+                                                className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                placeholder="Capacité"
+                                                value={hotelForm.capacity}
+                                                onChange={(e) => setHotelForm({...hotelForm, capacity: e.target.value})}
+                                            />
+
                                         </div>
                                         <div>
                                             <button
